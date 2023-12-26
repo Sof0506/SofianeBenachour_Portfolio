6eac43fed5216bad486458b3d7e0dc16a2694acf
SofBen <106510033+Sof0506@users.noreply.github.com>
2023-12-27 00:19:57 +0100

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,60 +1,58 @@
 import Link from 'next/link';
-import React from 'react'
-import {Cursor,useTypewriter} from "react-simple-typewriter"
+import React from 'react';
+import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import { urlFor } from '../sanity';
 import { PageInfo } from '../typings';
 import BackgroundCircles from './BackgroundCircles';
-type Props = {
-    pageInfo:PageInfo;
-}
 
-export default function Hero({pageInfo}: Props) {
-    const [text,count]=useTypewriter({
-        words:[`Hi, it's me, ${pageInfo?.name}`,
-        'Guy-who-loves-Coffe.tsx',
-        ' <ButLovesToCodeMore>'],
-        loop:true,
-        delaySpeed:2000,
-    });
+type Props = {
+  pageInfo: PageInfo;
+};
+
+export default function Hero({ pageInfo }: Props) {
+  const [text, count] = useTypewriter({
+    words: [
+      `Hi, it's me, ${pageInfo?.name}`,
+      'Guy-who-loves-Coffee.tsx',
+      '<ButLovesToCodeMore>',
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
 
   return (
-    <div className='h-screen flex flex-col space-y-8 items-center 
-    justify-center text-center overflow-hidden'>
-        <div className='mt-28'>
-        <BackgroundCircles/>
-        </div>
-      
-       <img 
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+      <div className='mt-28'>
+        <BackgroundCircles />
+      </div>
+
+      <img
         className='relative rounded-full h-32 w-32 mx-auto object-cover'
-src={urlFor(pageInfo?.profilePic).url()}
-/>
-    
-        
-        <div className='z-20'>
-            <h2 className='text-sm uppercase text-gray-500 pb-2 
-            tracking-[20px]'>{pageInfo.role}</h2>
+        src={urlFor(pageInfo?.profilePic).url()}
+        alt='Profile Pic'
+      />
+
+      <div className='z-20'>
+        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[20px]'>
+          {pageInfo.role}
+        </h2>
         <h1 className='text-xl lg:text-2xl font-semibold px-10'>
-        <span className='mr-3'>{text}</span>
-        <Cursor cursorColor='#F7AB0A'/>
+          <span className='mr-3'>{text}</span>
+          <Cursor cursorColor='#F7AB0A' />
         </h1>
         <div className='pt-12'>
-            <Link href="#about">
+          <Link href='#about'>
             <button className='heroButton'>About</button>
-            </Link>
-            
-            <Link href="#skills">
+          </Link>
+
+          <Link href='#skills'>
             <button className='heroButton'>Skills</button>
-            </Link>
-            <Link href="#projects">
+          </Link>
+          <Link href='#projects'>
             <button className='heroButton'>Projects</button>
-
-            </Link>
-           
+          </Link>
         </div>
-        </div>
-        
-       
-
+      </div>
     </div>
-  )
+  );
 }
